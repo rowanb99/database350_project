6017daa5a5d613568f62a0f4e27cbc51b7dbdff5
Matthew Taylor <111338980+porcupineparty@users.noreply.github.com>
2024-02-22 13:18:31 -0700

    Update insertAutoInc.

--- insertScript.sql
+++ insertScript.sql
@@ -1,7 +1,7 @@
-INSERT INTO user
+INSERT INTO user (UserFName, UserLName, UserEmail, UserPassword, Username)
 VALUES
-(102, 'Alice', 'Johnson', 'alice.j@example.com', 'alicepassword', 'alice92'),
-(103, 'Bob', 'Jones', 'bobjones@email.com', 'bobpassword', 'bobby123'),
-(104, 'Emma', 'Williams', 'emma.w@example.com', 'emmapassword', 'emwill'),
-(105, 'Michael', 'Brown', 'michael.b@example.com', 'mikepassword', 'mbrown88');
+('Alice', 'Johnson', 'alice.j@example.com', 'alicepassword', 'alice92'),
+('Bob', 'Jones', 'bobjones@email.com', 'bobpassword', 'bobby123'),
+('Emma', 'Williams', 'emma.w@example.com', 'emmapassword', 'emwill'),
+('Michael', 'Brown', 'michael.b@example.com', 'mikepassword', 'mbrown88');
 select * from user
