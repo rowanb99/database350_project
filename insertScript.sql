a9c1da73d7b3e0e13cc7403f841b23e22d8b931b
Matthew Taylor <111338980+porcupineparty@users.noreply.github.com>
2024-02-22 13:41:40 -0700

    Update insertScript.sql semicolon

--- insertScript.sql
+++ insertScript.sql
@@ -1,7 +1,12 @@
+-- Delete and Insert
 INSERT INTO user (UserFName, UserLName, UserEmail, UserPassword, Username)
 VALUES
 ('Alice', 'Johnson', 'alice.j@example.com', 'alicepassword', 'alice92'),
 ('Bob', 'Jones', 'bobjones@email.com', 'bobpassword', 'bobby123'),
 ('Emma', 'Williams', 'emma.w@example.com', 'emmapassword', 'emwill'),
 ('Michael', 'Brown', 'michael.b@example.com', 'mikepassword', 'mbrown88');
-select * from user
+select * from user;
+
+DELETE FROM user 
+WHERE UserID = 2;
+
