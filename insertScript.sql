c0f0f93a231fc74504a086efc846dc4f3e160c05
mburov99 <90662012+mburov99@users.noreply.github.com>
2024-02-22 12:58:56 -0700

    Add files via upload

---
+++ insertScript.sql
@@ -0,0 +1,7 @@
+INSERT INTO user
+VALUES
+(102, 'Alice', 'Johnson', 'alice.j@example.com', 'alicepassword', 'alice92'),
+(103, 'Bob', 'Jones', 'bobjones@email.com', 'bobpassword', 'bobby123'),
+(104, 'Emma', 'Williams', 'emma.w@example.com', 'emmapassword', 'emwill'),
+(105, 'Michael', 'Brown', 'michael.b@example.com', 'mikepassword', 'mbrown88');
+select * from user
